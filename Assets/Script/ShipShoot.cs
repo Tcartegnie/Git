9a518c11f707001ac2f5dff3ed07113485bedf18
Théo1 <Sorakssis@gmail.com>
2019-09-30 22:59:08 +0200

    Add ShipShoot class
    Add new class to split the code of the project

---
+++ Assets/Script/ShipShoot.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ShipShoot : ScriptableObject
+{
+	public Canon LeftCanon;
+	public Canon RightCanon;
+	public void upadte()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Shoot();
+		}
+		RotateCanon(new Vector3());
+	}
+
+
+	public void Shoot()
+	{
+		LeftCanon.Shoot(0);
+		RightCanon.Shoot(0);
+	}
+
+
+	public void RotateCanon(Vector3 Direction)
+	{
+		//Vector3 RayDirection = shpashipUI.GetCursorRay().direction;
+		//		Vector3 TargetPos = transform.position + (RayDirection * CrossPointDistance);
+
+
+
+		LeftCanon.TargetPoint(Direction);
+		RightCanon.TargetPoint(Direction);
+	}
+
+}
+
+
