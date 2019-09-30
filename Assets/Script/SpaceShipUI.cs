17d8bb45fe0999e2a8e5746fe882cca9318d26db
Théo1 <Sorakssis@gmail.com>
2019-09-30 23:00:02 +0200

    Add life bar, shield bar, and speed bar.

--- Assets/Script/SpaceShipUI.cs
+++ Assets/Script/SpaceShipUI.cs
@@ -12,6 +12,12 @@ public class SpaceShipUI : MonoBehaviour
 
 	public Vector2 DistanceCursorMax;
 
+	public RectTransform SpeedBarMask;
+
+	public RectTransform LifeBarMask;
+
+	public RectTransform ShieldBarMask;
+
 	private void Start()
 	{
 		Cursor.lockState = CursorLockMode.Locked;
@@ -25,6 +31,9 @@ public class SpaceShipUI : MonoBehaviour
 
 		MoveCursor(mouseAxis.x, mouseAxis.y);
 
+		SetSpeedBar();
+		SetLifeBar();
+		SetShieldBar();
 		//Debug.DrawRay(shipController.transform.position, GetCursorRay().direction * 1000, Color.red);
 	} 
 
@@ -40,6 +49,27 @@ public class SpaceShipUI : MonoBehaviour
 	public Ray GetCursorRay()//This must go in the part of the script
 	{
 		return ShipController.currentCamera.ScreenPointToRay(ShipCursor.position);
+	}
+
+
+	public void SetSpeedBar()
+	{
+		float NormalizedVelocity = shipController.GetNormalizedVelocity();
+		//Debug.Log(NormalizedVelocity);
+		SpeedBarMask.anchorMax = new Vector2(NormalizedVelocity,1);
+	}
+
+
+	public void SetLifeBar()
+	{
+		float NormalizedLife =	shipController.GetNormalizedLife();
+		LifeBarMask.anchorMax = new Vector2(NormalizedLife, 0.5f);
+	}
+
+	public void SetShieldBar()
+	{
+		float NormalizedShield = shipController.GetNormalizedShield();
+		ShieldBarMask.anchorMax = new Vector2(NormalizedShield, 0.5f);
 	}
 
 }
