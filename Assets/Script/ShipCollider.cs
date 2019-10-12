5397f964f0da7f3c65210020b2f25c71a031fa2d
Tcartegnie <Sorakssis@gmail.com>
2019-10-13 00:57:51 +0200

    Add a new class ship collider
    This class will manage the collision of the ships

---
+++ Assets/Script/ShipCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipCollider : Ship
+{
+	public void OnTriggerEnter(Collider other)
+	{
+		Debug.Log(gameObject.name);
+		Debug.Log(other.tag + " is triggering with " + gameObject.name);
+		if (other.tag == "Bullet")
+		{
+			ShipState.OnHit(other.GetComponentInParent<Bullet>().damage);
+			Destroy(other.gameObject);
+		}
+	}
+
+}
