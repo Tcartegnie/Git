3f80f4d530acf0b336ca2acea2da37f5ae2b59c5
Tcartegnie <Sorakssis@gmail.com>
2019-10-13 00:57:18 +0200

    Add a new class ship

---
+++ Assets/Script/Ship.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+
+	public ShipState ShipState;
+	// Start is called before the first frame update
+	void Start()
+    {
+		ShipState.Gameover += OnGameOver;
+	}
+
+	public virtual void OnGameOver()
+	{ }
+
+}
