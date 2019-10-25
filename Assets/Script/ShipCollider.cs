9b6379606a73ead80f7c37d6e09a130c028aa3b9
Tcartegnie <Sorakssis@gmail.com>
2019-10-25 14:32:06 +0200

    Add a script to manage the collision of every ships

--- Assets/Script/ShipCollider.cs
+++ Assets/Script/ShipCollider.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class ShipCollider : Ship
 {
+	public string HitBulletTag;
 	public void OnTriggerEnter(Collider other)
 	{
 		Debug.Log(gameObject.name);
 		Debug.Log(other.tag + " is triggering with " + gameObject.name);
-		if (other.tag == "Bullet")
+		if (other.tag == HitBulletTag)
 		{
 			ShipState.OnHit(other.GetComponentInParent<Bullet>().damage);
 			Destroy(other.gameObject);
