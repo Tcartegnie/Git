ce85a873153875a892bef6e9f9c84817a97de4d9
Tcartegnie <Sorakssis@gmail.com>
2019-10-25 14:30:58 +0200

    Add a NPC in the game

---
+++ Assets/Script/AIShoot.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIShoot : MonoBehaviour
+{
+	public GameObject target;
+	public float RangeDetection;
+	public float DistanceDetection;
+	public List<Canon> Canons;
+	public List<Turret> Turrets;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(CheckTargetInRange())
+		{
+			TurretsLookat();
+			ShootEnnemy();
+		}
+		else
+		{
+			TurretStopShoot();
+		}
+	}
+
+	public float GetTargetPositionInRange()
+	{
+		Vector3 pos = target.transform.position - transform.position;
+		float DotProduct = Vector3.Dot(transform.forward, Vector3.Normalize(pos));
+		return DotProduct;
+	}
+
+	public float GetDistanceFromTarget()
+	{
+		float distance = Vector3.Distance(target.transform.position, transform.position);
+		return distance;
+	}
+
+	public bool CheckTargetInRange()
+	{
+		if ((GetTargetPositionInRange() > RangeDetection) && (GetDistanceFromTarget() < DistanceDetection))
+			return true;
+		else
+			return false;
+	}
+
+	public void ShootEnnemy()
+	{
+		foreach(Canon canon in Canons)
+		{
+			canon.Shoot(0);
+		}
+	}
+
+	public void TurretsLookat()
+	{
+		foreach (Turret turret in Turrets)
+		{
+			turret.LookAtTarget(target.transform);
+			if(target.layer == 9)
+			{
+				turret.CallShoot();
+			}
+		}
+	}
+
+	public void TurretStopShoot()
+	{
+		foreach (Turret turret in Turrets)
+		{
+			turret.StopShoot();
+		}
+	}
+
+
+
+
+}
---
+++ Assets/Script/ScoutIAMove.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ScoutIAMove : MonoBehaviour
+{
+	public Transform target;
+	public float RayCastOffset;
+	public float RayCastRange;
+	public float rotationRange;
+	public float Speed;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+		MoveInDirection(transform.forward);
+		FollowTarget();
+    }
+
+
+	void Turn()
+	{
+		Vector3 pos = target.position - transform.position;
+		Quaternion rotation = Quaternion.LookRotation(pos);
+		transform.rotation = Quaternion.Slerp(transform.rotation,rotation, rotationRange * Time.deltaTime);
+	}
+
+	public void MoveInDirection(Vector3 Direction)
+	{
+		transform.position += Direction * (Time.deltaTime*Speed); 
+	}
+
+	public void FollowTarget()
+	{
+	
+		Vector3 Right = transform.position + transform.right * RayCastOffset;
+		Vector3 Left = transform.position - transform.right * RayCastOffset;
+		Vector3 Up = transform.position + transform.up * RayCastOffset;
+		Vector3 Down = transform.position - transform.up * RayCastOffset;
+
+		RaycastHit hit = new RaycastHit();
+
+		Vector3 rayCastOffset = Vector3.zero;
+
+		if (Physics.Raycast(Right, transform.forward, out hit, RayCastRange))
+		{
+			rayCastOffset -= Vector3.right;
+		}
+		else if (Physics.Raycast(Left, transform.forward, out hit, RayCastRange))
+		{
+			rayCastOffset += Vector3.right;
+		}
+
+		if (Physics.Raycast(Up,transform.forward,out hit,RayCastRange ))
+		{
+			rayCastOffset += Vector3.up;
+		}
+		else if (Physics.Raycast(Down, transform.forward, out hit, RayCastRange))
+		{
+			rayCastOffset -= Vector3.up;
+		}
+
+
+		if (rayCastOffset != Vector3.zero)
+			transform.Rotate(rayCastOffset * 5f * Time.deltaTime);
+		else
+			Turn();
+	}
+}
