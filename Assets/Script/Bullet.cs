76cd36b1ba916e7cf7d185a791e93c441c198c0a
Tcartegnie <Sorakssis@gmail.com>
2019-11-06 15:44:20 +0100

    Now the bullet move by velocity and not by position

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -7,18 +7,36 @@ public class Bullet : MonoBehaviour
 	float speed;
     float damage;
 	public float lifetime;
-
+	Rigidbody RB;
+	public float SpeedMax;
 	public float Speed { get => speed; set => speed = value; }
 
+
+
+	public void Start()
+	{
+		RB = GetComponent<Rigidbody>();
+		MoveForward();
+	}
 	// Start is called before the first frame update
 	public void Update()
 	{
-		transform.position += transform.forward * (Time.deltaTime * Speed);
+		MoveForward();
 		lifetime -= Time.deltaTime;
-		if(lifetime < 0)
+		CheckLife();
+	}
+
+	public void CheckLife()
+	{
+		if (lifetime < 0)
 		{
 			Destroy(gameObject);
 		}
+	}
+
+	public void MoveForward()
+	{
+		RB.velocity = transform.forward *  Speed;
 	}
 
 	public void SetDamage(float damage)
