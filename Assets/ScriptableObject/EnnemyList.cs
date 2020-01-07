b9771ca5597031e3a6a97c69c54a85036743ae81
Tcartegnie <Sorakssis@gmail.com>
2020-01-07 19:26:16 +0100

    Add a new SOBJ

---
+++ Assets/ScriptableObject/EnnemyList.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "EnnemyList", menuName = "EnnemyList", order = 1)]
+public class EnnemyList : ScriptableObject
+{
+	[SerializeField]
+    List<ShipType> shipTypes = new List<ShipType>();
+	public List<ShipType>GetShips()
+	{
+		return shipTypes;
+	}
+}
