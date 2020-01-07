e2fc15d54259b77429225bf11a57b5b40c68bde2
Tcartegnie <Sorakssis@gmail.com>
2020-01-07 19:26:04 +0100

    Add a game manager

---
+++ Assets/Script/Game/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public ShipFactory shipFactory;
+	static GameManager instance;
+	public GameObject Player;
+	public AsteroïdField asteroidfield;
+	public static GameManager Instance { get => instance; set => instance = value; }
+	[SerializeField]
+	EnnemyList ennemyList;
+
+	public List<Transform> Spawns = new List<Transform>();
+
+	public List<GameObject> InstanciedEntitys = new List<GameObject>();
+
+	public void Start()
+	{
+		InstanciateEnnemiesWaves();
+	}
+
+	public void InstanciateEnnemiesWaves()
+	{
+		List<ShipType> currentEnnemyList = ennemyList.GetShips();
+		for(int i = 0; i < currentEnnemyList.Count; i++)
+		{
+			shipFactory.InstantiateShip(currentEnnemyList[i], Spawns[Random.Range(0,Spawns.Count)].position);
+		}
+
+		InstanciedEntitys = shipFactory.GetEnnemiesList();
+
+		for(int i = 0; i < InstanciedEntitys.Count;i++)
+		{
+			InstanciedEntitys[i].GetComponent<IAMove>().target = Player.transform;
+			InstanciedEntitys[i].GetComponent<IAMove>().colliders = asteroidfield.Asteroids;
+			InstanciedEntitys[i].GetComponent<AiAttack>().target = Player;
+		}
+	}
+}
