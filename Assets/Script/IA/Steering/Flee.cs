b2aade1f18bca1e5c8decfb5d99c9aa5ce483b35
Tcartegnie <Sorakssis@gmail.com>
2020-01-07 11:16:35 +0100

    Add flee steering behaviours

---
+++ Assets/Script/IA/Steering/Flee.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flee : Steering
+{
+	public Flee(Transform entityTr, Transform targetTr, Rigidbody RB, float MaxSpeed, float Wheight) : base(entityTr, targetTr, RB, MaxSpeed, Wheight) { }
+
+	public 	Vector3 ComputeSteering()
+	{
+		Vector3 desiredVelocity = (EntityTr.position - TargetTr.position).normalized * MaxSpeed;
+		Vector3 steering = desiredVelocity - Rb.velocity;//Get velocity to apply
+		return steering;
+	}
+
+	public Vector3 ComputeSteering(Vector3 targetPosition)
+	{
+		Vector3 desiredVelocity = (EntityTr.position - targetPosition).normalized * MaxSpeed;
+		Vector3 steering = desiredVelocity - Rb.velocity;//Get velocity to apply
+		return steering;
+	}
+
+	public override Vector3 GetSteering()
+	{
+		return ComputeSteering();
+	}
+}
