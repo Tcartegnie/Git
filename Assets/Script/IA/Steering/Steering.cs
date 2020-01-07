d710002fe856dc3ad46ed7d3c037d4c559243964
Tcartegnie <Sorakssis@gmail.com>
2020-01-07 11:15:06 +0100

    Add steering behaviour

---
+++ Assets/Script/IA/Steering/Steering.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Steering
+{
+	protected Transform EntityTr;
+	protected Transform TargetTr;
+	protected Rigidbody Rb;
+	protected float MaxSpeed;
+	protected float Wheight;
+
+ protected Steering(Transform EntityTr,Transform TargetTr,Rigidbody Rb,float MaxSpeed,float Wheight)
+	{
+		this.EntityTr = EntityTr;
+		this.TargetTr = TargetTr;
+		this.Rb = Rb;
+		this.MaxSpeed = MaxSpeed;
+		this.Wheight = Wheight;
+	}
+
+	abstract public Vector3 GetSteering();
+	public float GetWheight()
+	{
+		return Wheight;
+	}
+}
