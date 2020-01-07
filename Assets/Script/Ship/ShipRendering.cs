0eae941716f9b0e20d8df6b9d8edcf381798e14c
Tcartegnie <Sorakssis@gmail.com>
2020-01-07 19:28:55 +0100

    add new script to check if an object is rendered

---
+++ Assets/Script/Ship/ShipRendering.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipRendering : MonoBehaviour
+{
+	public delegate void Visibility();
+
+	public Visibility visibility;
+	public Visibility Novisibility;
+
+	private void OnBecameVisible()
+	{
+		Debug.Log("Im in the camera");
+		visibility?.Invoke();
+	}
+
+	private void OnBecameInvisible()
+	{
+		Debug.Log("Im out of the camera");
+		Novisibility?.Invoke();
+	}
+
+
+
+}
