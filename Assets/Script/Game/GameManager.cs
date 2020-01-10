2273c2136dbee5a3d5bfc12e9e0387f84d2af36a
Tcartegnie <Sorakssis@gmail.com>
2020-01-10 14:51:37 +0100

    Refacto the game manager

--- Assets/Script/Game/GameManager.cs
+++ Assets/Script/Game/GameManager.cs
@@ -4,38 +4,84 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-	public ShipFactory shipFactory;
-	static GameManager instance;
-	public GameObject Player;
-	public AsteroïdField asteroidfield;
-	public static GameManager Instance { get => instance; set => instance = value; }
-	[SerializeField]
-	EnnemyList ennemyList;
 
-	public List<Transform> Spawns = new List<Transform>();
+	public ShipFactory shipFactory;
+	public GameObject Player;
+
+	public AsteroïdField asteroidfield;
+
+	public static GameManager instance;
+
+	public delegate void OnVictory();
+	public delegate void OnGameOver();
+
+	public OnVictory onVictory;
+	public OnGameOver onGamover;
+
+
+	[SerializeField]
+	List<EnnemyList> ennemyWave;
+
+	int WaveIndex = 0;
+
 
 	public List<GameObject> InstanciedEntitys = new List<GameObject>();
 
+	public void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+		else if(instance!= null)
+		{
+			Destroy(instance);
+		}
+
+		Player.GetComponentInParent<ShipState>().ShipDestroy += EndGameGameOver;
+
+	}
+
 	public void Start()
 	{
-		InstanciateEnnemiesWaves();
+		shipFactory.onEnnemyListEmpty += OnEnnemyWaveDone;
+		InstanciateWaves(WaveIndex);
 	}
 
-	public void InstanciateEnnemiesWaves()
+	public void InstanciateWaves(int WaveID)
 	{
-		List<ShipType> currentEnnemyList = ennemyList.GetShips();
-		for(int i = 0; i < currentEnnemyList.Count; i++)
-		{
-			shipFactory.InstantiateShip(currentEnnemyList[i], Spawns[Random.Range(0,Spawns.Count)].position);
-		}
-
+		shipFactory.InstantiateWave(ennemyWave[WaveID].GetShips());
 		InstanciedEntitys = shipFactory.GetEnnemiesList();
+		shipFactory.InitEnnemies(Player,asteroidfield);
+	}
+	  
+	public void InstantiateNextWave()
+	{
+		WaveIndex += 1;
+		InstanciateWaves(WaveIndex);
+	}
 
-		for(int i = 0; i < InstanciedEntitys.Count;i++)
+	public void OnEnnemyWaveDone()
+	{
+		if ((WaveIndex +1) == ennemyWave.Count)
 		{
-			InstanciedEntitys[i].GetComponent<IAMove>().target = Player.transform;
-			InstanciedEntitys[i].GetComponent<IAMove>().colliders = asteroidfield.Asteroids;
-			InstanciedEntitys[i].GetComponent<AiAttack>().target = Player;
+			EndGameVictory();
+		}
+		else
+		{
+			InstantiateNextWave();
 		}
 	}
+
+	public void EndGameVictory()
+	{
+		onVictory?.Invoke();
+	}
+
+	public void EndGameGameOver(GameObject test)
+	{
+		onGamover?.Invoke();
+	}
+
+
 }
