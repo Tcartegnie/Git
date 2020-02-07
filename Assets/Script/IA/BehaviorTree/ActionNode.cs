87d97addcada455e38d539b0e1e5be2c4af54286
Tcartegnie <Sorakssis@gmail.com>
2020-02-07 15:31:22 +0100

    Add action node for behaviour tree

---
+++ Assets/Script/IA/BehaviorTree/ActionNode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionNode : BehaviorTreeNode, IParentBehaviourTreeNodes
+{
+	Func<float, BehaviourTreeStatus> function;
+
+	public ActionNode(string name, Func<float, BehaviourTreeStatus> fn) : base(name)
+	{
+		function = fn;
+	}
+
+	public void AddChild(BehaviorTreeNode child)
+	{
+		ChildNodes = child;
+	}
+
+
+	public override BehaviourTreeStatus Tick(float Time)
+	{
+		return function(Time);
+	}
+}
