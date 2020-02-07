364fa9d7c7f5c1077a72bde2f657585e54e371a1
Tcartegnie <Sorakssis@gmail.com>
2020-02-07 15:30:57 +0100

    Add a root node

---
+++ Assets/Script/IA/BehaviorTree/BehaviourTreeRoot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourTreeRoot : BehaviorTreeNode
+{
+	BehaviorTreeNode NextNode;
+
+	public BehaviourTreeRoot(string name,BehaviorTreeNode nextNode): base(name)
+	{
+		NextNode = nextNode;
+	}
+
+
+	public override BehaviourTreeStatus Tick(float Time)
+	{
+		return NextNode.Tick(Time);
+	}
+}
