b99a4c348c67daa0f5e0f741d20f0c09f32a5037
Tcartegnie <Sorakssis@gmail.com>
2020-02-07 15:30:37 +0100

    Add the selector in the behaviour tree

---
+++ Assets/Script/IA/BehaviorTree/BehaviourTreeSelector.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourTreeSelector :BehaviorTreeNode, IParentBehaviourTreeNodes
+{
+	private List<BehaviorTreeNode> Nodes = new List<BehaviorTreeNode>();
+	public BehaviourTreeSelector(string name) : base(name)
+	{
+
+	}
+
+	public void AddChild(BehaviorTreeNode child)
+	{
+		Nodes.Add(child);
+	}
+
+
+	
+
+	public override BehaviourTreeStatus Tick(float Time)
+	{
+		foreach (BehaviorTreeNode node in Nodes)
+		{
+			BehaviourTreeStatus BTS = node.Tick(Time);
+
+			if (BTS != BehaviourTreeStatus.Failed)
+			{
+				return BTS;
+			}
+		}
+		return BehaviourTreeStatus.Failed;
+	}
+}
