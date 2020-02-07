0660327143c8a63e2292fe056088225771ad8c3c
Tcartegnie <Sorakssis@gmail.com>
2020-02-07 15:30:24 +0100

    Add sequence in the behaviour tree

---
+++ Assets/Script/IA/BehaviorTree/BehaviourTreeSequence.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourTreeSequence : BehaviorTreeNode , IParentBehaviourTreeNodes
+{
+	private List<BehaviorTreeNode> Nodes = new List<BehaviorTreeNode>();
+	public BehaviourTreeSequence(string name) : base(name)
+	{
+
+	}
+
+
+
+	public override BehaviourTreeStatus Tick(float Time)
+	{
+		foreach(BehaviorTreeNode node in Nodes)
+		{
+			BehaviourTreeStatus BTS = node.Tick(Time);
+			
+			if(BTS != BehaviourTreeStatus.Succes)
+			{
+				return BTS;
+			}
+		}
+		return BehaviourTreeStatus.Succes;
+	}
+
+
+	public void AddChild(BehaviorTreeNode child)
+	{
+		Nodes.Add(child);
+	}
+}
