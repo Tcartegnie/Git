cb6425dd0fdd81086558730a8c3d09bfc0d3a552
Tcartegnie <Sorakssis@gmail.com>
2020-02-07 15:31:45 +0100

    Add condition node in the behaviour tree

---
+++ Assets/Script/IA/BehaviorTree/ConditionNode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConditionNode : BehaviorTreeNode, IParentBehaviourTreeNodes
+{
+	Stack<BehaviorTreeNode> StackBehavior;
+	BehaviorTreeNode ValideNextNode;
+	Func<float, bool> condition;
+
+	public ConditionNode(string name, Func<float,bool> condition) : base(name)
+	{
+		this.condition = condition;
+	}
+
+	public override BehaviourTreeStatus Tick(float Time)
+	{
+		if (condition(Time))
+		{
+			return BehaviourTreeStatus.Succes;
+		}
+		else
+		{
+			return	BehaviourTreeStatus.Failed;
+		}
+	}
+
+
+	public void AddChild(BehaviorTreeNode child)
+	{
+		ChildNodes = child;
+	}
+}
